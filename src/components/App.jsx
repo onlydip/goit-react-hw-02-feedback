ec3c5d45b9d76c7a488151a0e7808f82fd77f14f
Aleksandr Harianin <onlydipro@gmail.com>
2023-09-01 12:48:18 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-    goit-react-hw-02-feedback
-    </div>
-  );
-};
+import React, { Component } from "react";
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+
+
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <h1>Відгуки</h1>
+        <button>Класс</button>
+        <button>Нейтрально</button>
+        <button>Погано</button>
+        <h2>Статистика</h2>
+        <button>Класс:{good}</button>
+        <button>Нейтрально:{neutral}</button>
+        <button>Погано:{bad}</button>
+
+      </div>
+    )
+  }
+}
